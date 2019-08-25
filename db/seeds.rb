50631495ff8289714c7cae433c225dda2cf253bb
Brittany Javalera <brittjavalera@gmail.com>
2019-08-25 21:25:46 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
 require_relative "../lib/student.rb"
 
+# Student.create(name: "Melissa", grade: "10th")
+# Student.create(name: "April", grade: "10th")
+# Student.create(name: "Luke", grade: "9th")
+# Student.create(name: "Devon", grade: "11th")
+# Student.create(name: "Sarah", grade: "10th")
+namespace :db do
+ 
 Student.create(name: "Melissa", grade: "10th")
 Student.create(name: "April", grade: "10th")
 Student.create(name: "Luke", grade: "9th")
 Student.create(name: "Devon", grade: "11th")
 Student.create(name: "Sarah", grade: "10th")
-
+ 
+  desc 'seed the database with some dummy data'
+  task :seed do
+    require_relative './db/seeds.rb'
+  end
+end
 
